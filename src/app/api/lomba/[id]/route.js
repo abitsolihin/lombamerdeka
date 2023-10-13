8c2a40628b1e4d16fcbad6c577d2122edca76e27
Abit Solihin <abitsolihin8@gmail.com>
2023-10-13 19:54:16 +0700

    UPDATE: Menambahkan fitur count page

--- src/app/api/lomba/[id]/route.js
+++ src/app/api/lomba/[id]/route.js
@@ -23,13 +23,50 @@ export const GET = async (request, { params }) => {
         // Get the lomba data from the snapshot
         const lombaData = docSnapshot.data();
 
-        // Calculate total views (views)
-        const totalViews = lombaData.views + 1; // For example, we increment by 1 each time the lomba is accessed
+        // Create a success response with the lomba data and total views
+        const response = {
+            status: "success",
+            lomba: lombaData,
+        };
+        return new NextResponse(JSON.stringify(response), {
+            status: 200
+        });
+    } catch (error) {
+        // If an error occurs, return a server error response
+        return new NextResponse("Failed to Connect to Database", {
+            status: 500
+        });
+    }
+};
 
-        // Update views in Firestore (optional, if you want to persist the total views)
+export const PUT = async ({ params }) => {
+    try {
+        // Extract the ID from the route parameters
+        const { id } = params;
+
+        // Create a Firestore document reference using the extracted ID
+        const docRef = doc(db, "lombas", id);
+
+        // Get a snapshot (data) of the document
+        const docSnapshot = await getDoc(docRef);
+
+        // If the document does not exist, return a 404 response
+        if (!docSnapshot.exists()) {
+            return new NextResponse("Lomba not found", {
+                status: 404
+            });
+        }
+
+        // Get the lomba data from the snapshot
+        const lombaData = docSnapshot.data();
+
+        // Calculate total views (views)
+        const totalViews = lombaData.views + 1; // Increment views
+
+        // Update views in Firestore
         await updateDoc(docRef, { views: totalViews });
 
-        // Create a success response with the lomba data and total views
+        // Return a success response
         const response = {
             status: "success",
             lomba: lombaData,

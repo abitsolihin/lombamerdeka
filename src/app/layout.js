28bc54cd51d94395b1ed1276fee9f9c66564372f
Abit Solihin <abitsolihin8@gmail.com>
2023-10-16 00:02:55 +0700

    UPDATE : ADDING ADS

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,18 @@ import './globals.css'
 import AuthProvider from '@/components/AuthProvider/AuthProvider'
 import NextTopLoader from 'nextjs-toploader';
 import 'react-toastify/dist/ReactToastify.css';
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Script
+          id="adsbygoogle-init"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2066100267293064"
+        />
         <NextTopLoader color='#000000' />
         <AuthProvider>
           <main className='min-h-screen flex justify-center mx-4 md:mx-24'>

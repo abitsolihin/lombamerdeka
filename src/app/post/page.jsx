0513e417b6302a4ce14cf0598428da6c804fcd9f
Abit Solihin <abitsolihin8@gmail.com>
2023-10-18 18:19:44 +0700

    UPDATE: FIX BUTTON FILTER

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -5,19 +5,37 @@ import { authOption } from "../api/auth/[...nextauth]/route";
 import ModalLogin from "@/components/Modal/ModalLogin";
 
 const Post = async () => {
-    const session = await getServerSession(authOption)
+  const session = await getServerSession(authOption);
 
-    if (session) {
-        return <div className="flex justify-center w-full min-h-screen">
-            <FormPost />
-            <Image className="absolute bottom-0 left-0 right-0 w-full -z-20" src={'/flag.svg'} alt="balap karung" width='378' height='378' priority />
-        </div>
-    } else {
-        return <div className="flex items-center justify-center min-h-screen">
+  if (session) {
+    return (
+      <div className="flex justify-center w-full min-h-screen">
+        <FormPost />
+        <Image
+          className="absolute bottom-0 left-0 right-0 w-full -z-20"
+          src={"/flag.svg"}
+          alt="balap karung"
+          width="378"
+          height="378"
+          priority
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
         <ModalLogin />
-        <Image className="absolute bottom-0 left-0 right-0 w-full -z-20" src={'/flag.svg'} alt="balap karung" width='378' height='378' priority />
-    </div>
-    }
+        <Image
+          className="absolute bottom-0 left-0 right-0 w-full -z-20"
+          src={"/flag.svg"}
+          alt="balap karung"
+          width="378"
+          height="378"
+          priority
+        />
+      </div>
+    );
+  }
 };
 
 export default Post;
--- src/components/Button/ButtonFilter.jsx
+++ src/components/Button/ButtonFilter.jsx
@@ -11,10 +11,10 @@ const ButtonFilter = () => {
     }
 
     return <div className="right flex items-center flex-1 justify-between sm:justify-end gap-4">
-        <span onClick={() => handleClick('')} className={query.kategori === undefined ? "cursor-pointer text-white bg-slate-900 px-2 py-1 rounded" : "cursor-pointer text-slate-900"}>All</span>
-        <span onClick={() => handleClick('Trend TikTok')} className={query.kategori === "Trend TikTok" ? "cursor-pointer text-white bg-slate-900 px-2 py-1 rounded" : "cursor-pointer text-slate-900"}>Trend TikTok</span>
-        <span onClick={() => handleClick('Dewasa')} className={query.kategori === "Dewasa" ? "cursor-pointer text-white bg-slate-900 px-2 py-1 rounded" : "cursor-pointer text-slate-900"}>Dewasa</span>
-        <span onClick={() => handleClick('Anak-Anak')} className={query.kategori === "Anak-Anak" ? "cursor-pointer text-white bg-slate-900 px-2 py-1 rounded" : "cursor-pointer text-slate-900"}>Anak-Anak</span>
+        <button onClick={() => handleClick('')} className={query.kategori === undefined ? "cursor-pointer text-white bg-slate-900 px-2 py-1 rounded" : "cursor-pointer text-slate-900"}>All</button>
+        <button onClick={() => handleClick('Trend TikTok')} className={query.kategori === "Trend TikTok" ? "cursor-pointer text-white bg-slate-900 px-2 py-1 rounded" : "cursor-pointer text-slate-900"}>Trend TikTok</button>
+        <button onClick={() => handleClick('Dewasa')} className={query.kategori === "Dewasa" ? "cursor-pointer text-white bg-slate-900 px-2 py-1 rounded" : "cursor-pointer text-slate-900"}>Dewasa</button>
+        <button onClick={() => handleClick('Anak-Anak')} className={query.kategori === "Anak-Anak" ? "cursor-pointer text-white bg-slate-900 px-2 py-1 rounded" : "cursor-pointer text-slate-900"}>Anak-Anak</button>
     </div>;
 };
 
